5480893684f5751182b8d7efb52de0051070e19a
Briso10-dev <kouambrice10@gmail.com>
2025-03-09 19:19:46 +0100

    App file importting all other components implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/items/Navbar.tsx'
+import Home from './pages/Home.tsx'
+import Challenges from './pages/Challenges.tsx'
+import IAMCLI from './pages/IamCli.tsx'
+import './index.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+     <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/challenges" element={<Challenges />} />
+        <Route path="/challenges/iam-cli" element={<IAMCLI />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 
     </>
   )
