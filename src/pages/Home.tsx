3934175bef931ae26065056ec2bb1b4ad8192e51
Briso10-dev <kouambrice10@gmail.com>
2025-03-11 23:51:04 +0100

    Header content changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@
 function Home(){
     return (
       <div className="flex flex-col items-center justify-center h-screen text-center px-6">
-        <h1 className="text-3xl md:text-5xl font-bold">Welcome to AWS Challenges</h1>
+        <h1 className="text-3xl md:text-5xl font-bold">Welcome to Briso's AWS Challenges</h1>
         <p className="text-gray-600 mt-2 text-lg md:text-xl">
           Practice AWS Hands-On Challenges to master the cloud.
         </p>
