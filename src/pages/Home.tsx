609cdfd5103f31439d9aaa074dff26b6540fd206
Briso10-dev <kouambrice10@gmail.com>
2025-03-13 22:08:07 +0100

    Home page commented

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 
+// Here the home page
 function Home(){
     return (
       <div className="flex flex-col items-center justify-center h-screen text-center px-6">
